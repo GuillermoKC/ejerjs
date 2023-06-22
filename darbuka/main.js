611eccd86eb7a9088f56f763aaac278bcbc4b027
GuillermoKC <guillermokencp@gmail.com>
2023-06-22 19:27:10 +0200

    darbika fin

---
+++ darbuka/main.js
@@ -0,0 +1,198 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyW") { 
+      reproducirSonidoW();
+      aplicarAnimacionW();
+    }
+  });
+  function aplicarAnimacionW() {
+    var div = document.getElementById("wclick");
+    div.classList.add("animacion");
+    
+    setTimeout(function() {
+      div.classList.remove("animacion");
+    }, 500);
+};
+  function reproducirSonidoW() {
+    var sonido = document.getElementById("sonidodarkutaW");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyA") {
+        reproducirSonidoA();
+        aplicarAnimacionA();
+        
+      }
+    });
+    function aplicarAnimacionA() {
+        var div = document.getElementById("aclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoA() {
+    var sonido = document.getElementById("sonidodarkutaA");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyD") {
+        reproducirSonidoD();
+        aplicarAnimacionD();
+        
+      }
+    });
+    function aplicarAnimacionD() {
+        var div = document.getElementById("dclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoD() {
+    var sonido = document.getElementById("sonidodarkutaD");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyS") {
+        reproducirSonidoS();
+        aplicarAnimacionS();
+        
+      }
+    });
+    function aplicarAnimacionS() {
+        var div = document.getElementById("sclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoS() {
+    var sonido = document.getElementById("sonidodarkutaS");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyE") {
+        reproducirSonidoE();
+        aplicarAnimacionE();
+        
+      }
+    });
+    function aplicarAnimacionE() {
+        var div = document.getElementById("eclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoE() {
+    var sonido = document.getElementById("sonidodarkutaS");
+    sonido.play();
+  }
+    document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyE") {
+        reproducirSonidoE();
+        aplicarAnimacionE();
+        
+      }
+    });
+    function aplicarAnimacionE() {
+        var div = document.getElementById("eclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoE() {
+    var sonido = document.getElementById("sonidodarkutaS");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyQ") {
+        reproducirSonidoQ();
+        aplicarAnimacionQ();
+        
+      }
+    });
+    function aplicarAnimacionQ() {
+        var div = document.getElementById("qclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoQ() {
+    var sonido = document.getElementById("sonidodarkutaQ");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyR") {
+        reproducirSonidoR();
+        aplicarAnimacionR();
+        
+      }
+    });
+    function aplicarAnimacionR() {
+        var div = document.getElementById("rclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoR() {
+    var sonido = document.getElementById("sonidodarkutaR");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyF") {
+        reproducirSonidoF();
+        aplicarAnimacionF();
+        
+      }
+    });
+    function aplicarAnimacionF() {
+        var div = document.getElementById("fclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoF() {
+    var sonido = document.getElementById("sonidodarkutaF");
+    sonido.play();
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.code === "KeyG") {
+        reproducirSonidoG();
+        aplicarAnimacionG();
+        
+      }
+    });
+    function aplicarAnimacionG() {
+        var div = document.getElementById("gclick");
+        div.classList.add("animacion");
+        
+        setTimeout(function() {
+          div.classList.remove("animacion");
+        }, 500);
+  };
+  
+  function reproducirSonidoG() {
+    var sonido = document.getElementById("sonidodarkutaG");
+    sonido.play();
+  } 
